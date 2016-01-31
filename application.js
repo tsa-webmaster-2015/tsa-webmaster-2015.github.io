88726d888713110082e15a3f3ac5d3e58b72ff27
ekyl <nkdigiulio@gmail.com>
2016-01-30 19:12:53 -0700

    rework url changer

--- application.js
+++ application.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
     $('nav').hide().fadeIn();
-    $('#content').load('/partials/home.html');
+    var page = location.search.substring(1, 2000);
+    if (page) {
+        $('#content').load('/partials/' + page + '.html');
+    } else {
+        $('#content').load('/partials/home.html');
+    }
     $('#content').fadeIn();
     console.log("    ____   ____   ____   _   __ ______ _____\n   / __ \\ / __ \\ / __ \\ / | / // ____// ___/\n  / / / // /_/ // / / //  |/ // __/   \\__ \\ \n / /_/ // _, _// /_/ // /|  // /___  ___/ / \n/_____//_/ |_| \\____//_/ |_//_____/ /____/  \n                                            ")
     $('nav').find('span').click(function () {
         id = this.id
         $('#content').load('/partials/' + id + '.html').hide().fadeIn();
+        window.history.pushState("string", "Title", "/?" + id);
     });
     $(window).on('scroll', function () {
         if ($(window).scrollTop() >= 100) {
@@ -18,5 +24,5 @@ $(document).ready(function () {
         $('body').animate({
             scrollTop: '0px'
         }, 100);
-    })
+    });
 });
