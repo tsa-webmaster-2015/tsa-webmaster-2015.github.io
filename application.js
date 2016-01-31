cc2df2008e8a7f91139ff6b98bf50d14be4160f5
ekyl <nkdigiulio@gmail.com>
2016-01-31 01:04:08 -0700

    NOTHING TO SEE HERE

--- application.js
+++ application.js
@@ -25,4 +25,13 @@ $(document).ready(function () {
             scrollTop: '0px'
         }, 100);
     });
+    var kkeys = [],
+        konami = "38,38,40,40,37,39,37,39,66,65";
+    $(document).keydown(function (e) {
+        kkeys.push(e.keyCode);
+        if (kkeys.toString().indexOf(konami) >= 0) {
+            $(document).unbind('keydown', arguments.callee);
+            window.location = '//ayylmao.rocks'
+        }
+    });
 });
